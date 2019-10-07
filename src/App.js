8dff7af9932e741b00a8130ac25aebfff2484b4a
Brandon Knight <bknight@springbuk.com>
2019-10-07 16:06:05 -0400

    ok

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  div {
+    font-size: 14px;
+  }
+  .App-header {
+    font-size: 20px;
+  }
+`;
+
+function App() {
+  return (
+    <AppContainer>
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </AppContainer>
+  );
+}
+
+export default App;
